f99ee09a4e1b58457997a907bffb0d65909aa29e
Amaan <amaanhussain1706@gmail.com>
2026-07-05 18:38:17 +0530

    implement RabbitMQ and Notification Service

--- order-service/src/main/java/com/cloudshop/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/cloudshop/order_service/controller/OrderController.java
@@ -1,10 +1,11 @@
 package com.cloudshop.order_service.controller;
 
-import com.cloudshop.order_service.config.ProductClient;
+import com.cloudshop.order_service.service.ProductClient;
 import com.cloudshop.order_service.dto.OrderConfirmationResponse;
 import com.cloudshop.order_service.dto.ProductResponse;
 import com.cloudshop.order_service.entity.Order;
 import com.cloudshop.order_service.repository.OrderRepository;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,10 +17,14 @@ public class OrderController {
 
     private final OrderRepository orderRepository;
     private final ProductClient productClient;
+    private final RabbitTemplate rabbitTemplate;
 
-    public OrderController(OrderRepository orderRepository, ProductClient productClient) {
+    public OrderController(OrderRepository orderRepository,
+                           ProductClient productClient,
+                           RabbitTemplate rabbitTemplate) {
         this.orderRepository = orderRepository;
         this.productClient = productClient;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
     @GetMapping
@@ -35,6 +40,10 @@ public class OrderController {
         // Process and save the order to the H2 database
         order.setOrderNumber(UUID.randomUUID().toString());
         Order savedOrder = orderRepository.save(order);
+
+        // SEND THE MESSAGE TO RABBITMQ!
+        String notificationMessage = "Order " + savedOrder.getOrderNumber() + " placed for " + product.name();
+        rabbitTemplate.convertAndSend("order.notifications", notificationMessage);
 
         return new OrderConfirmationResponse(
                 savedOrder.getOrderNumber(),
