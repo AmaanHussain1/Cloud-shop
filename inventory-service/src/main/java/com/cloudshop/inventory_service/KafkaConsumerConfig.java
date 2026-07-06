a488e2e04a75a50ec67bcccc7c0e24915abf7f5e
Amaan <amaanhussain1706@gmail.com>
2026-07-07 00:14:25 +0530

    implement Docker Compose and fix networking

--- inventory-service/src/main/java/com/cloudshop/inventory_service/KafkaConsumerConfig.java
+++ inventory-service/src/main/java/com/cloudshop/inventory_service/KafkaConsumerConfig.java
@@ -1,6 +1,7 @@
 package com.cloudshop.inventory_service;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -16,11 +17,14 @@ import java.util.Map;
 @Configuration
 public class KafkaConsumerConfig {
 
+    @Value("${spring.kafka.bootstrap-servers:localhost:9092}")
+    private String bootstrapServers;
+
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
         // Point to our Docker Kafka container
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "inventory-group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
